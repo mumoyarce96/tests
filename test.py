7878ae42c46257b32e0cfdda7667908274bd6790
Raimundo Oyarce Andrade <82007842+mumoyarce96@users.noreply.github.com>
2024-04-05 20:30:05 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from datetime import datetime
 
 dict = {'a': [1, 2, 3],
         'b': [13, 124, 231]}
 df = pd.DataFrame(dict)
+df['datetime'] = datetime.now()
 
 df.to_csv('test.csv')
