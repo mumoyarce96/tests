03078489864c453e3ad9fafa99e071cd7236f150
Raimundo Oyarce Andrade <82007842+mumoyarce96@users.noreply.github.com>
2024-04-05 22:04:27 -0300

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 
 url = 'https://raw.githubusercontent.com/mumoyarce96/tests/main/test.csv'
-prev_df = pd.read_csv(url, index_col = False)
+prev_df = pd.read_csv(url, index_col = False).drop('Unnamed: 0', axis = 1)
 
 dict = {'a': [1, 2, 3],
         'b': [13, 124, 231]}
@@ -10,4 +10,4 @@ dict = {'a': [1, 2, 3],
 df = pd.DataFrame(dict)
 df['datetime'] = datetime.now()
 df = pd.concat([prev_df, df]).reset_index(drop = True)
-df.to_csv('test.csv')
+df.to_csv('test.csv', index = False)
