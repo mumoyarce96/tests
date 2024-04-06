b6b45ca7e18351ac201a469a02418dc24bdc8b67
Raimundo Oyarce Andrade <82007842+mumoyarce96@users.noreply.github.com>
2024-04-05 21:56:47 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from datetime import datetime
 
+url = 'https://raw.githubusercontent.com/mumoyarce96/tests/main/test.csv'
+prev_df = pd.read_csv(url)
+
 dict = {'a': [1, 2, 3],
         'b': [13, 124, 231]}
 
 df = pd.DataFrame(dict)
 df['datetime'] = datetime.now()
+df = pd.concat([prev_df, df])
 df.to_csv('test.csv')
